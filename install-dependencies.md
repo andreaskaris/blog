9f416a6305441f3fdd31f6c771d7086515b9fc4a
Andreas Karis <ak.karis@gmail.com>
2023-03-22 17:10:26 +0100

    new commit

--- install-dependencies.md
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install mkdocs
-pip install mkdocs-material
---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eux
+
+pip install mkdocs
+pip install mkdocs-material
+pip install mkdocs-git-revision-date-localized-plugin
+pip install mkdocs-rss-plugin
+pip install mkdocs-blogging-plugin
